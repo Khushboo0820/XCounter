7020f3a72c1f849c6139abf9682e2ea469a9aa97
unknown <khushboo.parate74@gmail.com>
2024-10-16 22:32:12 +0530

    Final Commit

--- xcounter/src/App.js
+++ xcounter/src/App.js
@@ -2,22 +2,24 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const[count,setCount]=useState(0);
+  const [count, setCount] = useState(0);
 
-  const increment=()=>{
-    setCount(count+1);
+  const increment = () => {
+    setCount((prevCount) => prevCount + 1);
   };
-  const decrement=()=>{
-    setCount(count-1);
+
+  const decrement = () => {
+    setCount((prevCount) => prevCount - 1);
   };
-return(
-  <div>
-    <h1>Counter App</h1>
-    <p>Count : {count}</p>
-    <button onClick={increment}>Increment</button>
-    <button onClick={decrement}>Decrement</button>
-  </div>
-);
+
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
 }
 
 export default App;

8b3f218b8c416dd233ca38b8789b204f3cda5482
unknown <khushboo.parate74@gmail.com>
2024-10-16 22:16:36 +0530

    Final Commit

--- xcounter/src/App.js
+++ xcounter/src/App.js
@@ -12,7 +12,7 @@ function App() {
   };
 return(
   <div>
-    <h1 className="heading">Counter App</h1>
+    <h1>Counter App</h1>
     <p>Count : {count}</p>
     <button onClick={increment}>Increment</button>
     <button onClick={decrement}>Decrement</button>
